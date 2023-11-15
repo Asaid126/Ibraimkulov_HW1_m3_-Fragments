8331acb4756aec2ceea0348e6b0e62b182211c01
Asaid126 <ibraimkulov126>
2023-11-15 23:04:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ibraimkulov_hw1_m3__fragments/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ibraimkulov_hw1_m3__fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new MainFragment()).commit();
+    }
+}
